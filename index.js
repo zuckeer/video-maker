bb6e61295b49ec31d293a5a8cd5ff77ee7ce3c2e
Marcos Alves <zuckeer@gmail.com>
2022-08-16 22:56:56 -0300

    feat: start text robot with open source community support

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
 const readline = require('readline-sync')
+const Parser = require('rss-parser');
+const unicode = require('unidecode');
+const robots = {
+  text: require('./robots/text.js'),
+  gTrends: require('./robots/gTrends.js'),
+  wikipedia : require('./robots/wikipedia.js')
+}
 
-function start() {
-	const content = {}
+async function start(){
+  const content = {}
+  const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR' 
 
-	content.searchTerm = askAndReturnSearchTerm()
-	content.prefix = askAndReturnPrefix()
+  content.searchTerm = await askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
+  content.wikiPediaContent = await robots.wikipedia(content)
+  await robots.text(content)
+  console.log('To the next step...')
+  console.log('Exiting Program...')
 
-	function askAndReturnSearchTerm() {
-		return readline.question('Type a Wikipedia search term: ')
-	}
 
-	function askAndReturnPrefix() {
-		const prefixes = ['Who is', 'What is', 'The history of']
-		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Chose one option: ')
-		const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-		return selectedPrefixText
-
-		//console.log(selectedPrefixText)
-	}
-
-	console.log(content)
+  /*
+  *
+  * Estou utilizando também a ideia do @danielschmitz onde o mesmo faz uma sugestão de assuntos a serem pesquisados por meio do Google Trends.
+  * 
+  * Alem de estar fazendo todas as validações de se foi inderido o valor ou não
+  * 
+  */
+  async function askAndReturnSearchTerm () {
+      const response = readline.question('Type a Wikipedia search term or G to fetch google trends: ')
+  
+      var value = (response.toUpperCase() === 'G') ?  await askAndReturnTrend() : response
+      if(!value){
+        console.log('You don\'t defined any search term...')
+        console.log('Exiting Program...')
+        process.exit()
+      }
+      return value
+  
+  }
+  /*
+  *
+  * Estou utilizando o package unicode, para assim solucionar o problema do KeyInSelect do readline-sync de não imprimir UTF-8 .
+  * 
+  */
+  async function askAndReturnTrend() {
+      console.log('Please Wait...')
+      var trends = await getGoogleTrends()
+      const trendsB = trends.items.map(i => i.title.toString('utf8'))
+      trends = trends.items.map(i => unicode(i.title.toString('utf8')))
+      const choice = readline.keyInSelect(trends, 'Choose your trend:')
+  
+      return trendsB[choice] 
+  }
+  
+  async function getGoogleTrends () {
+      const parser = new Parser();
+      const trends = await parser.parseURL(TREND_URL);
+      return trends
+  }
+  
+  function askAndReturnPrefix(){
+      const prefix = ['Who is','What is','The history of']
+      const selectedPrefixIndex = readline.keyInSelect(prefix,'Choose an option for \''+unicode(content.searchTerm)+' \':')
+      const selectedPrefixText = prefix[selectedPrefixIndex]
+      if(!selectedPrefixText){
+        console.log('You don\'t defined a option for your term...')
+        console.log('Exiting Program...')
+        process.exit()
+      }
+      return selectedPrefixText
+  }
+  console.log(content)
 }
 
 start()
---
+++ robots/text.js
@@ -0,0 +1,150 @@
+// const algorithmia = require('algorithmia')
+// const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const sentenceBoundaryDetection = require('sbd')
+
+async function Text(content) {
+    // await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    breakContentIntoSentences(content)
+    console.log('Build Sentences')
+
+    /*
+    *
+    * Com o Robo da Wikipedia não precisamos utilizar o Algorithmia e usar nossos credito no mesmo.
+    * 
+    * Assim sendo passo diretamente os valores para o robo de texto e utlizo o que o robo do Wikipedia retornou para mim
+    * 
+    */
+    // async function fetchContentFromWikipedia(content) {
+    //   const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+    //   const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+    //   const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+    //   const wikipediaContent = wikipediaResponde.get()
+
+    //   content.sourceContentOriginal = wikipediaContent.content
+    // }
+
+    // function sanitizeContent(content) {
+    //     const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.wikiPediaContent.content)
+    //     const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+    //     content.wikiPediaContent.sourceContentSanitized = withoutDatesInParentheses
+
+    //     function removeBlankLinesAndMarkdown(text) {
+    //         const allLines = text.split('\n')
+
+    //         const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+    //             if (line.trim().length === 0 || line.trim().startsWith('=')) {
+    //                 return false
+    //             }
+
+    //             return true
+    //         })
+
+    //         return withoutBlankLinesAndMarkdown.join(' ')
+    //     }
+    // }
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.wikiPediaContent.content)// content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        //content.sourceContentSanitized = withoutDatesInParentheses
+        content.wikiPediaContent.sourceContentSanitized = withoutDatesInParentheses
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                return false
+                }
+
+            return true
+        })
+        
+        return withoutBlankLinesAndMarkdown.join(' ')
+        }
+    }
+
+    function removeDatesInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
+    }
+
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+        const sentences = sentenceBoundaryDetection.sentences(content.wikiPediaContent.sourceContentSanitized)
+        sentences.forEach((sentence) => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
+    }
+
+}
+
+module.exports = Text
+/******************************
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const sentenceBoundaryDetection = require('sbd')
+
+async function robot(content) {
+    await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    //breakContentIntoSentences(content)
+
+    async function fetchContentFromWikipedia(content) {
+        //console.log('Recebi com sucesso ${content.searchTerm}')
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
+        console.log(wikipediaContent)
+
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        content.sourceContentSanitized = withoutDatesInParentheses
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                return false
+                }
+
+            return true
+        })
+        
+        return withoutBlankLinesAndMarkdown.join(' ')
+        }
+    }
+
+    function removeDatesInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+    }
+
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+    sentences.forEach((sentence) => {
+      content.sentences.push({
+        text: sentence,
+        keywords: [],
+        images: []
+      })
+    })
+  }
+}
+
+module.exports = robot
+*/
